93831f6dd6595520c60cbcb37cea5e3b429a5da0
s0ckboy <159456700+s0ckboy@users.noreply.github.com>
2025-02-05 19:22:49 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "WAAAAA NOOOOO",
+    ":((((",
+    "HUHUHUHU",
+    "NOOOOOOO BABY",
 ];
 
 let messageIndex = 0;
